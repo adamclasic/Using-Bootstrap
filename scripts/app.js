459c7824b5fabd67a6c4d210fd7ee8b4b24f7ced
adam allalou <aabou9@gmail.com>
2020-03-06 10:56:56 +0100

    add main-section

--- scripts/app.js
+++ scripts/app.js
@@ -1,44 +1,44 @@
 $(document).ready(function () {
 
-    var ip = '';
-    $.ajax({
-        url: "https://api.ipify.org/?format=json",
-        cache: false,
-        async: false,
-        success: function(data) {
-            console.log(data)
-            ip = data.ip;
+    // var ip = '';
+    // $.ajax({
+    //     url: "https://api.ipify.org/?format=json",
+    //     cache: false,
+    //     async: false,
+    //     success: function(data) {
+    //         console.log(data)
+    //         ip = data.ip;
 
-        }
-    });
+    //     }
+    // });
 
-    var ipInfo = '';
-    var countryCode = '';
-    var cityName = '';
+    // var ipInfo = '';
+    // var countryCode = '';
+    // var cityName = '';
 
-    $.ajax({
-        url:"https://ipapi.co/" + ip + "/json/",
-        cashe: false,
-        async: false,
-        success: function(data) {
-            ipInfo = data;
-            countryCode = data.country_code;
-            cityName = data.region;
-            console.log(data);
-        }
-    });
+    // $.ajax({
+    //     url:"https://ipapi.co/" + ip + "/json/",
+    //     cashe: false,
+    //     async: false,
+    //     success: function(data) {
+    //         ipInfo = data;
+    //         countryCode = data.country_code;
+    //         cityName = data.region;
+    //         console.log(data);
+    //     }
+    // });
 
-    var temperature = '';
+    // var temperature = '';
 
-    $.ajax({
-        url:"http://api.openweathermap.org/data/2.5/weather?q=" + ipInfo.region + "&appid=98f5c39a38b987172eb484d62acb0f9c",
-        cach: false,
-        async: false,
-        success: function(data) {
-            temperature = data.main.temp - 273.15;
-            console.log(data);
-        }
-    });
+    // $.ajax({
+    //     url:"http://api.openweathermap.org/data/2.5/weather?q=" + ipInfo.region + "&appid=98f5c39a38b987172eb484d62acb0f9c",
+    //     cach: false,
+    //     async: false,
+    //     success: function(data) {
+    //         temperature = data.main.temp - 273.15;
+    //         console.log(data);
+    //     }
+    // });
 
     $('#search-box').focus(function() {
         
@@ -55,16 +55,16 @@ $(document).ready(function () {
         });
     
         $(window).scroll(function(){
-            $('.secendry-header').toggleClass('d-flex', $(this).scrollTop() > 125);
+            $('.secendry-header').toggleClass('d-flex', $(this).scrollTop() > 100);
         })
-        var today = new Date();
-        const options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' };
+        // var today = new Date();
+        // const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
 
-        var weatherData = temperature + '° ' + cityName + ', ' + countryCode;
-        var dateData = today.toLocaleDateString('us-EN', options);
+        // var weatherData = temperature + '° <span class="location">' + cityName + ', ' + countryCode + '</span>';
+        // var dateData = today.toLocaleDateString('us-EN', options);
         
-        $('.weather-data').text(weatherData);
-        $('.date-data').text(dateData);
+        // $('.weather-data').html(weatherData);
+        // $('.date-data').text(dateData);
 
     
     })
